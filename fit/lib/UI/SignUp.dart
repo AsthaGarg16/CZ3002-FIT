0d5aeac4d4dfadf96791f3b7293d3e194f9308f9
Aneez Ahmed Jaheezuddin <jaheezaneez55@gmail.com>
2022-02-12 17:25:47 +0800

    Added Sign Up UI

---
+++ fit/lib/UI/SignUp.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import './Login.dart';
+
+class SignupPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Sign Up", style: Theme.of(context).textTheme.subtitle1),
+        //Not sure if background of app bar should match sign up button
+        // backgroundColor: Colors.redAccent,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        titleSpacing: 0,
+        leading: IconButton(
+            onPressed: () {Navigator.pop(context);},
+            icon:Icon(Icons.arrow_back_ios,size: 20,color: Colors.white,)
+        ),
+      ),
+
+      body: SafeArea(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text ("Welcome!", style: Theme.of(context).textTheme.subtitle2,),
+                        const SizedBox(height: 20,),
+                        Text("Create an Account", style: Theme.of(context).textTheme.bodyText1,),
+                        SizedBox(height: 30,)
+                      ],
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          makeInput(label: "Email:"),
+                          makeInput(label: "Password:",obsureText: true),
+                          makeInput(label: "Confirm Pasword:",obsureText: true)
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: EdgeInsets.only(top: 3,left: 3),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(40),),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height:60,
+                          onPressed: (){},
+                          color: Colors.redAccent,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                          child: Text("Sign Up", style: Theme.of(context).textTheme.subtitle1,),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20,),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an account? "),
+                        Text("Login", style: Theme.of(context).textTheme.labelMedium),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+      ),
+    );
+  }
+}
--- fit/lib/main.dart
+++ fit/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './CreateAccount.dart';
-import 'Login.dart';
+import 'UI/SignUp.dart';
+import 'UI/Login.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         scaffoldBackgroundColor: const Color.fromRGBO(255, 255, 255, 1.0),
         primarySwatch: Colors.teal,
         primaryColor: Colors.teal[800],
@@ -32,7 +23,7 @@ class MyApp extends StatelessWidget {
           headline2: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black87, letterSpacing: 2.0),
           subtitle1: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white, letterSpacing: 1.5),
           subtitle2: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black87, letterSpacing: 1.5),
-          labelMedium: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black54, letterSpacing: 1.5),
+          labelMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black54, letterSpacing: 1.5),
           bodyText1: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal, color: Colors.black54),
           bodyText2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal, color: Colors.black54),
         ),
@@ -44,21 +35,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -66,31 +44,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title, style:Theme.of(context).textTheme.subtitle1),
       ),
       body: Center(
@@ -98,67 +55,48 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: Container(height: MediaQuery.of(context).size.height, width: double.infinity, padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
           child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height/4,
-              decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage('assets/images/food.png')),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height/4,
+                decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/images/food.png')),),
               ),
-            ),
-            SizedBox(height:50,),
+              SizedBox(height:50,),
 
-            Container(margin: const EdgeInsets.fromLTRB(40, 0, 40, 0),
-            child:MaterialButton(
-              minWidth: double.infinity,
-              height:60,
-              onPressed: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Login()));
-              },
-              color: Theme.of(context).colorScheme.primary,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40)
+              Container(
+                  margin: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                  child:MaterialButton(
+                    minWidth: double.infinity,
+                    height:60,
+                    onPressed: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Login()));
+                      },
+                    color: Theme.of(context).colorScheme.primary,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                    child: Text("Login", style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                  )
               ),
-
-              child: Text("Login", style: Theme.of(context).textTheme.subtitle1,
-                // style: TextStyle(
-                // fontWeight: FontWeight.w600,fontSize: 16,color:
-
-              ),) ),
-
-            // ),
-
+            //Gap between login and sign up button
             SizedBox(height: 30, ),
-            Container(margin: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+
+            Container(
+                margin: const EdgeInsets.fromLTRB(40, 0, 40, 0),
                 child: MaterialButton(
                   minWidth: double.infinity,
                   height:60,
                   onPressed: (){
-
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => SignupPage()
+                      )
+                    );
                   },
                   color: Colors.redAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40)
-                  ),
-                  child: Text("Sign Up", style: Theme.of(context).textTheme.subtitle1,
-
-                  ),)
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                  child: Text("Sign Up", style: Theme.of(context).textTheme.subtitle1,),
+                )
             ),
-
           ],
         ),
       )),
