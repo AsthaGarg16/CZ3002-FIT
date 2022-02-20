99fd505edaf461a008a08dee5807aceb7f8ba5f7
Astha Garg <garg0002@e.ntu.edu.sg>
2022-02-20 18:06:32 +0800

    Update ShopListCard.dart

--- fit/lib/UI/ShopListCard.dart
+++ fit/lib/UI/ShopListCard.dart
@@ -1,80 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LabeledCheckbox extends StatelessWidget {
-  const LabeledCheckbox({
-    Key? key,
-    required this.label,
-    required this.quantity,
-    required this.isRecipe,
-    required this.recipe,
-    required this.value,
-    required this.onChanged,
-  }) : super(key: key);
-
-  final String label;
-  final bool value;
-  final String quantity;
-  final bool isRecipe;
-  final String recipe;
-  final ValueChanged<bool> onChanged;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        decoration: const BoxDecoration(
-          border: Border(
-            top: BorderSide(color: Colors.black26),
-            bottom: BorderSide(color: Colors.black26),
-          ),),
-      child: Material(
-          child: InkWell(
-            onTap: () {
-              onChanged(!value);
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Checkbox(
-                      value: value,
-                      onChanged: (bool? newValue) {
-                        onChanged(newValue!);
-                      },
-                    ),
-                  ),
-
-                  const SizedBox(width: 5.0),
-                  Expanded(flex: 4,child: Text(label, style: const TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black87))),
-                  Expanded(
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        child: Text(quantity, style: const TextStyle(
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black87)),
-                      )
-
-                  ),
-                  const SizedBox(width: 2.0),
-                  Expanded(
-                    child: IconButton(icon: const Icon(Icons.arrow_drop_down), onPressed: () {  },),
-                  ),
-
-                ],
-              ),
-            ),
-          )
-      )
-    );
-  }
-}
-
 class ShopListWidget extends StatefulWidget {
-  const ShopListWidget({Key? key,
+  ShopListWidget({Key? key,
     required this.label,
     required this.quantity,
     required this.isRecipe,
@@ -84,24 +11,79 @@ class ShopListWidget extends StatefulWidget {
   final String quantity;
   final bool isRecipe;
   final String recipe;
+  bool value = false;
+  Color labelColor = Colors.black87;
 
   @override
   State<ShopListWidget> createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<ShopListWidget> {
-  bool _isSelected = false;
+
 
   @override
   Widget build(BuildContext context) {
-    return LabeledCheckbox(
-      label: widget.label,
-      value: _isSelected,
-      onChanged: (bool newValue) {
-        setState(() {
-          _isSelected = newValue;
-        });
-      }, recipe: widget.isRecipe ? widget.recipe:'', isRecipe: widget.isRecipe, quantity: widget.quantity,
+    bool _isSelected = false;
+
+    return Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.black26),
+            bottom: BorderSide(color: Colors.black26),
+          ),),
+        child: Material(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  _isSelected = !widget.value;
+                  widget.value=_isSelected;
+                  widget.labelColor = widget.value?Colors.teal:Colors.black87;
+                });
+
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Checkbox(
+                        value: widget.value,
+                        onChanged: (bool? newValue) {
+                          setState(() {
+                            _isSelected = newValue!;
+                            widget.value = _isSelected;
+                            widget.labelColor = widget.value?Colors.teal:Colors.black87;
+                          });
+                        },
+                      ),
+                    ),
+
+                    const SizedBox(width: 5.0),
+                    Expanded(flex: 4,child: Text(widget.label, style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.normal,
+                        color: widget.labelColor))),
+                    Expanded(
+                        child: Container(
+                          alignment: Alignment.centerRight,
+                          child: Text(widget.quantity, style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.normal,
+                              color: widget.labelColor)),
+                        )
+
+                    ),
+                    const SizedBox(width: 2.0),
+                    Expanded(
+                      child: IconButton(icon: const Icon(Icons.arrow_drop_down_rounded), onPressed: () {  },),
+                    ),
+
+                  ],
+                ),
+              ),
+            )
+        )
     );
+
   }
 }
