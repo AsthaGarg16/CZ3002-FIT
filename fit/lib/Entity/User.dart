43dc6ddd8ad03e44cb20014fc7484f0c8ec302c2
nisha-snr <nisha007@e.ntu.edu.sg>
2022-02-21 17:29:02 +0800

    Create User.dart
    added user class

---
+++ fit/lib/Entity/User.dart
@@ -0,0 +1,72 @@
+
+// need to check if date of birth and target range have been added to account details, profilepage and food preference
+/// Entity representing the user, their personal details, and the logbooks, plans and reminders associated with them.
+class User {
+  String _email;
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+  }
+
+  String _name;
+  int  _fridgeDetails;
+  bool _dairyFree;
+  bool _glutenFree;
+  bool _vegetarian;
+  bool _vegan;
+
+  User(
+      String name,
+        String email,
+        int fridgeDetails,
+        bool dairyFree,
+        bool glutenFree,
+        bool vegetarian,
+        bool vegan
+        )
+      : _name = name,
+        _email = email,
+       _fridgeDetails=fridgeDetails,
+       _dairyFree=dairyFree,
+       _glutenFree=glutenFree,
+       _vegan=vegan,
+       _vegetarian=vegetarian;
+
+  String get name => _name;
+
+  set name(String value) {
+    _name = value;
+  }
+
+  int get fridgeDetails => _fridgeDetails;
+
+  set fridgeDetails(int value) {
+    _fridgeDetails = value;
+  }
+
+  bool get dairyFree => _dairyFree;
+
+  set dairyFree(bool value) {
+    _dairyFree = value;
+  }
+
+  bool get glutenFree => _glutenFree;
+
+  set glutenFree(bool value) {
+    _glutenFree = value;
+  }
+
+  bool get vegetarian => _vegetarian;
+
+  set vegetarian(bool value) {
+    _vegetarian = value;
+  }
+
+  bool get vegan => _vegan;
+
+  set vegan(bool value) {
+    _vegan = value;
+  }
+}
