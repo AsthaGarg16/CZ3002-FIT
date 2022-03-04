5b09fe8ccdac43de9b6a9dbe308d2dceb383488e
Astha Garg <garg0002@e.ntu.edu.sg>
2022-03-04 23:05:30 +0800

    updated shoppinglist dialog box
    Now can update list when entered in dialog box

---
+++ fit/lib/Entity/Units.dart
@@ -0,0 +1,8 @@
+class Units{
+  static const kg ='kg';
+  static const g ='g';
+  static const l ='l';
+  static const ml ='ml';
+  static const count ='count';
+  static const units = [kg,g,l,ml,count];
+}
--- fit/lib/UI/ShoppingList.dart
+++ fit/lib/UI/ShoppingList.dart
@@ -1,7 +1,10 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import '../Entity/FoodItem.dart';
+import '../Entity/Units.dart';
 import 'ShopListCard.dart';
+import 'ShopListDialogUtils.dart';
 
 class ShoppingList extends StatefulWidget {
   List<Map<String, Object>> shopList = [
@@ -81,16 +84,16 @@ class _ShopListState extends State<ShoppingList> {
         resizeToAvoidBottomInset: false,
 
         body: Container(
-            height: MediaQuery.of(context).size.height,
+            //height: MediaQuery.of(context).size.height,
             width: double.infinity,
             child: Column(
               children: <Widget>[
                 Expanded(
-                    child: Scrollbar(
-                        isAlwaysShown: true,
+                    child:SingleChildScrollView(
                         child: Column(
                           children: <Widget>[
                         ListView.builder(
+                            controller: ScrollController(),
                         shrinkWrap: true,
                             itemCount: widget.shopList.length,
                             itemBuilder: (context, index) {
@@ -115,6 +118,7 @@ class _ShopListState extends State<ShoppingList> {
                               );
                             }),
                             ListView.builder(
+                                controller: ScrollController(),
                                 shrinkWrap: true,
                                 itemCount: widget.shopListChecked.length,
                                 itemBuilder: (context, index) {
@@ -133,19 +137,255 @@ class _ShopListState extends State<ShoppingList> {
                                   );
                                 })
                           ]
-                        )
+                        ))
 
 
 
-            )),]),),
+            ),]),),
                   floatingActionButton: FloatingActionButton(
                     onPressed: () {
-                      //_showMaterialDialog();
-                      // Add your onPressed code here!
+                      // showCustomDialog(context).then((valueFromDialog){
+                      //   // use the value as you wish
+                      //   print(valueFromDialog);
+                      // });
+                      showCustomDialog(context);
+
                     },
 
                     child: const Icon(Icons.add, size:30.0),
                   )
     );
   }
+
+  showCustomDialog(BuildContext context,
+      {String title = "Add item",
+        String okBtnText = "Save",
+        String cancelBtnText = "Cancel" }) {
+
+    FoodItem newItem ;
+    String _name="";
+    int  _quantity=0;
+    String _unit = Units.units[0];
+    String? chosenValue;
+    bool _status=false;
+    bool _inventory_status=false;
+    bool _from_saved_recipes=false;
+    String _recipeID="";
+
+    final GlobalKey<FormState> _dialogformKey = GlobalKey<FormState>();
+
+    AlertDialog AddItemDialog = AlertDialog(
+      title: Text(title, style:Theme
+          .of(context)
+          .textTheme
+          .subtitle2,textAlign: TextAlign.center,),
+      content: Container(
+        height: 270,
+        child: Column(
+          children: <Widget>[
+
+                  Form(
+                    key: _dialogformKey,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+
+                                Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Column(
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            "Item name: ",
+                                            style: TextStyle(
+                                                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          TextFormField(
+                                            obscureText: false,
+                                            decoration: InputDecoration(
+                                              enabledBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                  color: (Colors.grey[400])!,
+                                                ),
+                                              ),
+                                              border: OutlineInputBorder(
+                                                  borderSide: BorderSide(color: (Colors.grey[400])!)),
+                                            ),
+                                            style: const TextStyle(
+                                                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black87),
+
+                                            validator: (String? value) {
+                                              if (value != null && value.isEmpty) {
+                                                return "Please enter the name of the item";
+                                              }
+
+                                              return null;
+                                            },
+                                            onChanged: (val) {
+                                              setState(() => _name = val);
+                                            },
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            "Quantity: ",
+                                            style: TextStyle(
+                                                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          TextFormField(
+                                            obscureText: false,
+                                            decoration: InputDecoration(
+                                              enabledBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                  color: (Colors.grey[400])!,
+                                                ),
+                                              ),
+                                              border: OutlineInputBorder(
+                                                  borderSide: BorderSide(color: (Colors.grey[400])!)),
+                                            ),
+                                            style: const TextStyle(
+                                                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black87),
+
+                                            validator: (String? value) {
+                                              if (value != null && value.isEmpty) {
+                                                return "Please enter the quantity";
+                                              }
+                                              if (value != null && double.parse(value) == null) {
+                                                return "Please enter numeric quantity";
+                                              }
+
+                                              return null;
+                                            },
+                                            onChanged: (val) {
+                                              setState(() => _quantity = int.parse(val));
+                                            },
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          )
+                                        ],
+                                      ),
+
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            "Unit: ",
+                                            style: TextStyle(
+                                                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          DropdownButtonFormField<String>(
+                                              value: _unit,
+                                              //elevation: 5,
+                                              style: const TextStyle(
+                                                  fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black87),
+
+                                              items: Units.units.map<DropdownMenuItem<String>>((String value) {
+                                                return DropdownMenuItem<String>(
+                                                  value: value,
+                                                  child: Text(value, style: const TextStyle(
+                                                      fontFamily:"mw",fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black87),
+                                                  ),
+                                                );
+                                              }).toList(),
+                                              hint: const Text(
+                                                "Please choose a unit",
+                                                style: TextStyle(
+                                                    fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black87),
+                                              ),
+                                              onChanged: (String? value) {
+
+                                                setState(() {
+                                                  chosenValue=value;
+                                                });
+
+                                              }
+                                          ),
+
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                )]),
+        ),]),),
+
+      actions: <Widget>[
+        MaterialButton(
+          onPressed: () {
+            if(chosenValue!=null)
+            {
+              _unit = chosenValue??Units.units[0];
+            }
+            newItem = FoodItem(_name,_quantity,_unit,_status,_inventory_status,_from_saved_recipes,_recipeID);
+            Navigator.pop(context,newItem);
+          },
+          color: Theme
+              .of(context)
+              .colorScheme
+              .primary,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)),
+          child: Text(okBtnText, style: const TextStyle(fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white)),
+        ),
+        MaterialButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          color: Theme
+              .of(context)
+              .colorScheme
+              .primary,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)),
+          child: Text(cancelBtnText, style: const TextStyle(fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white)),
+        )
+      ],
+    );
+
+    Future futureValue = showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AddItemDialog;
+        }
+    );
+
+    futureValue.then( (value) {
+      setState(() {
+        Map<String,Object> newObj = {
+          'label': value.name,
+          'quantity': value.quantity,
+          'recipe': '',
+          'isRecipe' : false,
+          'value': false,
+        };
+        widget.shopList.add(newObj);
+      });
+      print(value);
+    });
+
+  }
+
+
+
 }
